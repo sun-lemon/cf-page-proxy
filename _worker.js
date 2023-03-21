b1e56dd2374b0785c203b1dc5dbbd399a5cae022
sun-lemon <64244854+sun-lemon@users.noreply.github.com>
2023-03-21 20:37:31 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,24 @@
 export default {
-  async fetch(request) {
-    const url = /^http(s)?:\/\//i.test(request.url) ? new URL(request.url) : new URL(`https://${request.url}`);
-    const queryString = decodeURIComponent(url.search.substring(1));
-    return await fetch(queryString);
-  }
-}
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      
+      const url = /^http(s)?:\/\//i.test(request.url) ? new URL(request.url) : new URL(`https://${request.url}`);
+      const queryString = decodeURIComponent(url.search.substring(1));
+      return fetch(queryString);
+      
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
+    }
+  };
+export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="btdig.com";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
+    }
+  };
